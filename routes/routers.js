518720e71d10e7345b6bf2ff9039342f93fc10d8
Lil-boss <rshaon09@gmail.com>
2022-05-25 14:52:47 +0600

    update

--- routes/routers.js
+++ routes/routers.js
@@ -2,44 +2,54 @@ const router = require('express').Router();
 const productController = require('../controller/ProductController');
 const userController = require('../controller/UserController');
 const orderController = require('../controller/OrdersController');
-const authHeader = require('../Authentication/auth');
-const authentication = require('../Middleware/middleware');
+const authHeader = require('..//auth');
 const paymentController = require('../controller/PaymentsController');
 const reviewController = require('../controller/ReviewsController');
+const jwt = require("jsonwebtoken");
 
+const authentication = async (req, res, next) => {
+    const author = req.headers.authorization;
+    if (!author) {
+        res.status(401).json({ message: 'authorization denied' });
+    }
+    const token = author.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.PRIVATE_KEY);
+    req.user = decoded
+    next();
+}
 //auth
 router.post("/auth", authHeader.auth);
 
 //payments
-router.post("/create-payment-intent", authentication.authentication, paymentController.createPayment);
+router.post("/create-payment-intent", paymentController.createPayment);
 
 //Products
-router.get("/products", authentication.authentication, productController.getProducts);
-router.get("/product/:id", authentication.authentication, productController.getProductById);
-router.post("/products", authentication.authentication, productController.createProduct);
-router.put("/product/:id", authentication.authentication, productController.updateProduct);
-router.delete("/product/:id", authentication.authentication, productController.deleteProduct);
+router.get("/products", authentication, productController.getProducts);
+router.get("/product/:id", productController.getProductById);
+router.post("/products", productController.createProduct);
+router.put("/product/:id", productController.updateProduct);
+router.delete("/product/:id", productController.deleteProduct);
 
 //Users
-router.get("/users", authentication.authentication, userController.getAllUsers);
-router.get("/user/:email", authentication.authentication, userController.getUserByEmail);
-router.post("/users", authentication.authentication, userController.createUser);
-router.get("/user/:id", authentication.authentication, userController.getUserById);
-router.put("/user/:id", authentication.authentication, userController.updateUser);
-router.delete("/user/:id", authentication.authentication, userController.deleteUser);
+router.get("/users", userController.getAllUsers);
+router.get("/user/:email", userController.getUserByEmail);
+router.post("/users", userController.createUser);
+router.get("/user/:id", userController.getUserById);
+router.put("/user/:id", userController.updateUser);
+router.delete("/user/:id", userController.deleteUser);
 
 
 //orders
-router.get("/orders", authentication.authentication, orderController.getOrders);
-router.get("/order/:id", authentication.authentication, orderController.getOrderById);
-router.post("/orders", authentication.authentication, orderController.createOrder);
-router.put("/order/:id", authentication.authentication, orderController.updateOrder);
-router.delete("/order/:id", authentication.authentication, orderController.deleteOrder);
+router.get("/orders", orderController.getOrders);
+router.get("/order/:id", orderController.getOrderById);
+router.post("/orders", orderController.createOrder);
+router.put("/order/:id", orderController.updateOrder);
+router.delete("/order/:id", orderController.deleteOrder);
 
 //reviews
-router.get("/reviews", authentication.authentication, reviewController.getReviews);
-router.get("/review/:id", authentication.authentication, reviewController.getReviewById);
-router.post("/reviews", authentication.authentication, reviewController.addReview);
-router.delete("/review/:id", authentication.authentication, reviewController.deleteReview);
+router.get("/reviews", reviewController.getReviews);
+router.get("/review/:id", reviewController.getReviewById);
+router.post("/reviews", reviewController.addReview);
+router.delete("/review/:id", reviewController.deleteReview);
 
 module.exports = router;
